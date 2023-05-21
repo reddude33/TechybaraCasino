634b9baba14523c5bf41dd9a8055bc857ff0fab8
reddude33 <131456629+reddude33@users.noreply.github.com>
2023-05-21 15:26:50 +0800

    Update index.tsx

--- src/games/Mines/index.tsx
+++ src/games/Mines/index.tsx
@@ -6,7 +6,7 @@ export default {
   name: 'Mines',
   short_name: 'mines',
   description: '',
-  creator: 'DwRFGbjKbsEhUMe5at3qWvH7i8dAJyhhwdnFoZMnLVRV',
+  creator: 'Hsz3QMdcKpXYfaNW6k2CidbGrLtCLFkawmPsr4Wq5UBF',
   theme_color: '#ff6a6a',
   image,
   app: React.lazy(() => import('./App')),
